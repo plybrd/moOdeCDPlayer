d48525d8e9c67a4487894f5d013a9ce7ff043343
Eric Coissac <eric@coissac.eu>
2021-05-14 09:24:50 +0200

    installs the new script musicbrainz.py and the required python libraries

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@ if [ ! -f /var/www/command/moode.php ]; then
     echo "In case you didn't notice, this doesn't look like a moOde installation."
     exit 1
 fi
+sudo apt -y install python3-musicbrainzngs python3-requests python3-libdiscid
 sudo cp addaudiocd.sh /usr/local/bin/addaudiocd.sh
 sudo cp remaudiocd.sh /usr/local/bin/remaudiocd.sh
+sudo cp musicbrainz.py /usr/local/bin/musicbrainz.py
 sudo cp addaudiocd.service /etc/systemd/system/addaudiocd.service
 sudo cp remaudiocd.service /etc/systemd/system/remaudiocd.service
 sudo cp 99-srX.rules /etc/udev/rules.d/99-srX.rules
 sudo chmod +x /usr/local/bin/addaudiocd.sh
 sudo chmod +x /usr/local/bin/remaudiocd.sh
+sudo chmod +x /usr/local/bin/musicbrainz.py
 echo "Done - please reboot moOde to complete installation."
