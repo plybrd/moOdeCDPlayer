48aaa2494c4c1526c9dc11c42164d4ab6873e2db
Eric Coissac <eric@coissac.eu>
2021-05-14 09:18:33 +0200

    The new version of addaudiocd.sh builds a ExtM3u playlist loads and plays it.

--- addaudiocd.sh
+++ addaudiocd.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # moOdeCDPlay V0.1 script
 
-#clear the queue
-/usr/bin/mpc -q clear
 # make drive readable, just in case
 # (allow for odd enumeration)
 if [ -b /dev/sr0 ]; then
@@ -13,11 +11,18 @@ else
   exit 1
 fi
 
-#get the track count from udev
-tracks=$(udevadm info --query=property /dev/cdrom | \
-sed -n 's/ID_CDROM_MEDIA_TRACK_COUNT_AUDIO=\(.*\)/\1/p')
+#get the track count from cdparanoia
+musicbrainz.py > /var/lib/mpd/playlists/CDPlayer.m3u
+if [ ! -s /var/lib/mpd/playlists/CDPlayer.m3u ]; then
+  cdparanoia -vsQ 2>&1 | \
+      awk '/^TOTAL/ {on=0} (on) {print $1,$3} /^=+$/ {on=1}' | \
+      sed 's/\./:/' | \
+      sed -E 's/[ []|\]//g' | \
+      awk -F':' 'BEGIN {print "#EXTM3U";print ""}\
+                       {print "#EXTINF:" int($2*60+$3) ",CD Piste",$1 ; print "cdda:///"$1;print ""}' \
+      > /var/lib/mpd/playlists/CDPlayer.m3u
+fi
 
-#add each track to mpd queue 
-for ((i=1; i<=$tracks; i++)); do
-   /usr/bin/mpc -q add cdda:///$i
-done
+/usr/bin/mpc -q clear
+/usr/bin/mpc -q load CDPlayer
+/usr/bin/mpc -q play
