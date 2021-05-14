bd071ab735f3169e76e62e4ad11f0e1b7a5cd18b
Eric Coissac <eric@coissac.eu>
2021-05-14 09:23:57 +0200

    adds the -f option to rm in case the playlist doesn't exist

--- remaudiocd.sh
+++ remaudiocd.sh
@@ -2,7 +2,7 @@
 
 #clear the queue
 /usr/bin/mpc -q clear
-rm /var/lib/mpd/playlists/CDPlayer.m3u
+rm -f /var/lib/mpd/playlists/CDPlayer.m3u
 
 #The CD is ejected automatically when
 # the system detects the eject button is pressed.
